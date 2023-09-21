90ad32117ababc776427d4466d64dcf0a4d11bf0
MrBeam89 <117269111+MrBeam89@users.noreply.github.com>
2023-09-21 21:39:12 +0200

    Restructuré le code entier en fonctions
    + Fonctions
    + Docstrings
    - Variables globales

--- main.py
+++ main.py
@@ -3,11 +3,10 @@
 import requests
 import json
 
-identifiant = input("Identifiant: ")
-motdepasse = input("Mot de passe: ")
+# Pour l'emploi du temps et le cahier de texte
+date = "2023-09-22"
 
-LOGIN_URL = "https://api.ecoledirecte.com/v3/login.awp"
-login_request = f'data={{"uuid": "", "identifiant": "{identifiant}", "motdepasse": "{motdepasse}"}}'
+# En-tête, nécessaire pour toutes les requêtes
 header = {
             'authority': 'api.ecoledirecte.com',
             'accept': 'application/json, text/plain, */*',
@@ -22,71 +21,148 @@ header = {
             'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7'
         }
 
-# Tentative de connexion
-login_response = requests.post(url=LOGIN_URL, data=login_request, headers=header)
-login_response_json_data = login_response.json()
+# Connexion
+def login(identifiant, motdepasse):
+    '''
+    login : connexion avec identifiants EcoleDirecte
+    Arguments:
+    identifiant: votre identifiant EcoleDirecte
+    motdepasse: votre mot de passe EcoleDirecte
+    Renvoie (si connexion réussie):
+    [login_response_json_data, eleve_id, token]
+    login_response_json_data : données en JSON de votre compte EcoleDirecte
+    eleve_id : votre identifiant d'élève
+    token : votre token
+    '''
 
-# Obtient le token et l'identifiant d'élève
-token = login_response_json_data["token"]
-header["X-Token"] = token
-eleve_id = login_response_json_data["data"]["accounts"][0]["id"]
+    LOGIN_URL = "https://api.ecoledirecte.com/v3/login.awp"
+    login_request = f'data={{"uuid": "", "identifiant": "{identifiant}", "motdepasse": "{motdepasse}"}}'
+    # Tentative de connexion
+    login_response = requests.post(url=LOGIN_URL, data=login_request, headers=header)
+    login_response_json_data = login_response.json()
+
+    # Obtient le token et l'identifiant d'élève
+    token = login_response_json_data["token"] 
+    header["X-Token"] = token
+    eleve_id = login_response_json_data["data"]["accounts"][0]["id"]
+
+    return [login_response_json_data, eleve_id, token]
 
 # Timeline
-TIMELINE_URL = f'https://api.ecoledirecte.com/v3/eleves/{eleve_id}/timeline.awp?verbe=get'
-timeline_request = f'data={{"token": "{token}"}}'
+def timeline(eleve_id, token):
+    '''
+    timeline : Timeline
+    Arguments :
+    eleve_id : votre identifiant élève
+    token : votre token
+    Renvoie (si réussi) :
+    timeline_json_data : données en JSON de votre timeline
+    '''
 
-timeline_reponse = requests.post(url=TIMELINE_URL, data=timeline_request, headers=header)
-timeline_json_data = timeline_reponse.json()
+    TIMELINE_URL = f'https://api.ecoledirecte.com/v3/eleves/{eleve_id}/timeline.awp?verbe=get'
+    timeline_request = f'data={{"token": "{token}"}}'
+    timeline_reponse = requests.post(url=TIMELINE_URL, data=timeline_request, headers=header)
+    timeline_json_data = timeline_reponse.json()
+    return timeline_json_data
 
 # Emploi du temps
-EMPLOI_DU_TEMPS_URL = f"https://api.ecoledirecte.com/v3/E/{eleve_id}/emploidutemps.awp?verbe=get"
+def emploi_du_temps(eleve_id, token, date_debut, date_fin, avec_trous):
+    '''
+    emploi_du_temps : Emploi du temps
+    Arguments :
+    eleve_id : votre identifiant élève
+    date_debut : date de début de votre emploi du temps
+    date_fin : date de fin de votre emploi du temps
+    avec_trous : affiche ou pas les trous
+    /!\ avec_trous doit être soit "true" ou "false" (pas en booléen)
+    Renvoie (si réussi) :
+    emploi_du_temps_json_data : données en JSON de votre emploi du temps 
+    '''
 
-dateDebut = input("Emploi du temps (AAAA-MM-JJ): ")
-dateFin = dateDebut
-avecTrous = "false"
-emploi_du_temps_request = f'data={{"dateDebut": "{dateDebut}", "dateFin": "{dateFin}", "avecTrous": {avecTrous}}}'
-
-emploi_du_temps_reponse = requests.post(url=EMPLOI_DU_TEMPS_URL, data=emploi_du_temps_request, headers=header)
-emploi_du_temps_json_data = emploi_du_temps_reponse.json()
+    EMPLOI_DU_TEMPS_URL = f"https://api.ecoledirecte.com/v3/E/{eleve_id}/emploidutemps.awp?verbe=get"
+    emploi_du_temps_request = f'data={{"dateDebut": "{date_debut}", "dateFin": "{date_fin}", "avecTrous": {avec_trous}}}'
+    emploi_du_temps_reponse = requests.post(url=EMPLOI_DU_TEMPS_URL, data=emploi_du_temps_request, headers=header)
+    emploi_du_temps_json_data = emploi_du_temps_reponse.json()
+    return emploi_du_temps_json_data
 
 # Cahier de texte
-date_cahier_de_texte = input("Cahier de texte (AAAA-MM-JJ): ")
-CAHIER_DE_TEXTE_URL = f'https://api.ecoledirecte.com/v3/Eleves/{eleve_id}/cahierdetexte/{date_cahier_de_texte}.awp?v=3&verbe=get&'
-cahier_de_texte_request = f'data={{"token": "{token}"}}'
+def cahier_de_texte(eleve_id, token, date):
+    '''
+    cahier_de_texte : Cahier de texte
+    Arguments :
+    eleve_id : votre identifiant élève
+    token : votre token
+    date : jour de votre cahier de texte
+    Renvoie (si réussi) :
+    cahier_de_texte_json_data : données en JSON de votre cahier de texte
+    '''
 
-cahier_de_texte_reponse = requests.post(url=CAHIER_DE_TEXTE_URL, data=cahier_de_texte_request, headers=header)
-cahier_de_texte_json_data = cahier_de_texte_reponse.json()
+    CAHIER_DE_TEXTE_URL = f'https://api.ecoledirecte.com/v3/Eleves/{eleve_id}/cahierdetexte/{date}.awp?v=3&verbe=get&'
+    cahier_de_texte_request = f'data={{"token": "{token}"}}'
+    cahier_de_texte_reponse = requests.post(url=CAHIER_DE_TEXTE_URL, data=cahier_de_texte_request, headers=header)
+    cahier_de_texte_json_data = cahier_de_texte_reponse.json()
+    return cahier_de_texte_json_data
 
 # Notes
-NOTES_URL = f'https://api.ecoledirecte.com/v3/eleves/{eleve_id}/notes.awp?v=3&verbe=get&'
-notes_request = f'data={{"token": "{token}"}}'
+def notes(eleve_id, token):
+    '''
+    notes : Notes
+    Arguments :
+    eleve_id : votre identifiant élève
+    token : votre token
+    Renvoie (si réussi) :
+    notes_json_data : données en JSON de vos notes
+    '''
 
-notes_reponse = requests.post(url=NOTES_URL, data=notes_request, headers=header)
-notes_json_data = notes_reponse.json()
+    NOTES_URL = f'https://api.ecoledirecte.com/v3/eleves/{eleve_id}/notes.awp?v=3&verbe=get&'
+    notes_request = f'data={{"token": "{token}"}}'
+    notes_reponse = requests.post(url=NOTES_URL, data=notes_request, headers=header)
+    notes_json_data = notes_reponse.json()
+    return notes_json_data
 
 # Vie scolaire
-VIE_SCOLAIRE_URL = f'https://api.ecoledirecte.com/v3/eleves/{eleve_id}/viescolaire.awp?verbe=get'
-vie_scolaire_request = f'data={{"token": "{token}"}}'
+def vie_scolaire(eleve_id, token):
+    '''
+    vie_scolaire : Vie scolaire
+    Arguments :
+    eleve_id : votre identifiant élève
+    token : votre token
+    Renvoie (si réussi) :
+    vie_scolaire_json_data : données en JSON de votre vie scolaire
+    '''
 
-vie_scolaire_reponse = requests.post(url=VIE_SCOLAIRE_URL, data=vie_scolaire_request, headers=header)
-vie_scolaire_json_data = vie_scolaire_reponse.json()
+    VIE_SCOLAIRE_URL = f'https://api.ecoledirecte.com/v3/eleves/{eleve_id}/viescolaire.awp?verbe=get'
+    vie_scolaire_request = f'data={{"token": "{token}"}}'
+    vie_scolaire_reponse = requests.post(url=VIE_SCOLAIRE_URL, data=vie_scolaire_request, headers=header)
+    vie_scolaire_json_data = vie_scolaire_reponse.json()
+    return vie_scolaire_json_data
 
 # Pour tester
 print("\n")
 print("===LOGIN===")
-print(json.dumps(login_response_json_data, indent=4))
+login_out = login("NolanCV", "AZE.rty123")
+
+eleve_id = login_out[1]
+token = login_out[2]
+
+print(json.dumps(login_out[0], indent=4))
 print("\n")
+
 print("===TIMELINE===")
-print(json.dumps(timeline_json_data, indent=4))
+print(json.dumps(timeline(eleve_id, token), indent=4))
 print("\n")
+
 print("===EMPLOI DU TEMPS===")
-print(json.dumps(emploi_du_temps_json_data, indent=4))
+print(json.dumps(emploi_du_temps(eleve_id, token, date, date, "true"), indent=4))
 print("\n")
+
 print("===CAHIER DE TEXTE===")
-print(json.dumps(cahier_de_texte_json_data, indent=4))
+print(json.dumps(cahier_de_texte(eleve_id, token, date), indent=4))
 print("\n")
+
 print("===NOTES===")
-print(json.dumps(notes_json_data, indent=4))
+print(json.dumps(notes(eleve_id, token), indent=4))
 print("\n")
+
 print("===VIE SCOLAIRE===")
-print(json.dumps(vie_scolaire_json_data, indent=4))
+print(json.dumps(vie_scolaire(eleve_id, token), indent=4))
